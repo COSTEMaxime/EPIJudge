7bf0611062abba5e74c99f1088f8483df8c634bf
COSTEMaxime <maxime.coste@viacesi.fr>
2019-04-18 01:02:19 +0200

    Count bits + parity

--- epi_judge_cpp/count_bits.cc
+++ epi_judge_cpp/count_bits.cc
@@ -1,13 +1,21 @@
 #include "test_framework/generic_test.h"
 
 short CountBits(unsigned int x) {
-  // TODO - you fill in here.
-  return 0;
+
+	short sum = 0;
+
+	while (x)
+	{
+		sum += x & 1;
+		x >>= 1;
+	}
+
+	return sum;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"x"};
-  return GenericTestMain(args, "count_bits.cc", "count_bits.tsv", &CountBits,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "x" };
+	return GenericTestMain(args, "count_bits.cc", "count_bits.tsv", &CountBits,
+		DefaultComparator{}, param_names);
 }
